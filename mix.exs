ab9135c36a7e2d8e256380b958117e1e07744080
Fly Dev <dev@fly.io>
2025-12-14 00:51:28 +0000

    fix: add missing comma in deps list

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule OneTimeSecret.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}, # Added missing comma here
+      # Added missing comma here
+      {:bandit, "~> 1.5"},
       {:bcrypt_elixir, "~> 3.0"}
     ]
   end
