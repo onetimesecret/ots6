e6a0f0a8b906ddcfc7794771ed29e9980741245d
Fly Dev <dev@fly.io>
2025-12-14 00:51:12 +0000

    Fix mix.exs syntax error by adding comma between deps

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule OneTimeSecret.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"}, # Added missing comma here
       {:bcrypt_elixir, "~> 3.0"}
     ]
   end
